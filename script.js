86cc076efe6e593a9c3fdb3e2740304b2e0c3514
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2024-01-27 12:22:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
     //Write your code here
-  
+	
+	const form = document.getElementById('MyForm');
+	let r = Number(form.radius.value)
+	form.volume.value = (4*Math.PI*(r**3)) / 3;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
